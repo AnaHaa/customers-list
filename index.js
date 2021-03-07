16ebd4a8f89c3946a54c1fc1829d13178e88ca96
Antti Haarala <47451526+AnaHaa@users.noreply.github.com>
2021-03-07 14:30:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Include helmet
+const helmet = require('helmet');
+app.use(helmet());
+
+const port = 3000;
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${port}.`);
+});
+
+let customers = [
+  { id: '1588323375416', firstname: 'John', lastname: 'Johnson', email: 'john@johnson.com', phone: '8233243' },
+  { id: '1588323375417', firstname: 'Mary', lastname: 'Smith', email: 'mary@smith.com', phone: '6654113' },
+  { id: '1588323375418', firstname: 'Peter', lastname: 'North', email: 'peter@north.com', phone: '901176' },
+]
+
+app.get("/customers", (req, res) => {
+  res.render("customerlist", { customers: customers });
+})
+
+app.get("/", (req, res) => {
+  res.render("hello", { customers: customers });
+})
+
+app.get("/addcustomer", (req, res) => {
+  res.render("addcustomer");
+})
+
+app.post("/addcustomer", (req, res) => {
+  const newCustomer = { id: new Date().now, firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, phone: req.body.phone };
+  customers = [...customers, newCustomer];
+  res.redirect("/customers");
+})
